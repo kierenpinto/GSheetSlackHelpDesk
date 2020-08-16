378e24ffbaec0584a7c5cfeb0b9a90bd6c24925a
Kieren <kieren.pinto@hotmail.com>
2020-08-17 01:06:38 +1000

    test2

--- api/respond_to_question.js
+++ api/respond_to_question.js
@@ -1,8 +1,8 @@
 module.exports = (req, res) => {
     const payloadStr = req.body;
     console.log(payloadStr);
-    const payload = JSON.parse(payloadStr);
-    const id = payload.user.name;
+    const payload = JSON.parse(payloadStr.payload);
+    // const id = payload.user.name;
     
     
     res.status(200).send('Success')
